c957a7e1780b5f090d40905ab5f8b6c4aab214d2
tomni <tom.nijhof@cytosmart.com>
2018-03-16 16:42:48 +0100

    Get patient info. Error with the second fetch

--- php/get_patient.php
+++ php/get_patient.php
@@ -2,25 +2,45 @@
 session_start();
 require '../db.php';
 echo "hello world<br>";
-if ($stmt = $mysqli->prepare("SELECT client_id FROM accounts_clients WHERE account_id=?")) {
-    $user_id = $_SESSION['user_id'];
+// &&
+if ($stmt_acc_client = $mysqli->prepare("SELECT client_id FROM accounts_clients WHERE account_id=?")) {
+    if($stmt_client = $mysqli->prepare("SELECT first_name, last_name FROM clients WHERE id=?")){
+        $results = array();
 
-    $stmt->bind_param("i", $user_id);
-    $stmt->execute();
+        $user_id = $_SESSION['user_id'];
+        echo "user id $user_id<br>";
+        $stmt_acc_client->bind_param("i", $user_id);
+        $stmt_acc_client->execute();
+        $stmt_acc_client->bind_result($client_id);
+        /* bind result variables */
 
-    echo "user id is $user_id <br>";
-    /* bind result variables */
-    $stmt->bind_result($result);
+        //"SELECT first_name, last_name FROM clients WHERE id=?"
 
-    /* fetch value */
-    while($stmt->fetch()){
-        echo "client_id $result <br>";
+
+        /* fetch value */
+        while($stmt_acc_client->fetch()){
+            echo "client $client_id<br>";
+            $stmt_client->bind_param("i", $client_id);
+            $stmt_client->execute();
+            $stmt_client->bind_result($first_name, $last_name);
+
+            if($stmt_client->fetch()){
+                $result = array($user_id, $first_name, $last_name);
+                echo "$first_name<br>";
+                print_r($result);
+                echo " <br>";
+            }else{
+                echo "error<br>";
+            }
+        }
+    }else{
+        echo "ERROR: second statement";
     }
 
     /* close statement */
-    $stmt->close();
+    $stmt_acc_client->close();
 }else{
-    echo "I blame the unicorn";
+    echo "ERROR: first statement";
 }
 
 ?>
