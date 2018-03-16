c8b313e0dda58a5215d0baeb4be3925dc8fc3bbd
tomni <tom.nijhof@cytosmart.com>
2018-03-16 11:30:59 +0100

    Fixed fetching client_ids

--- php/get_patient.php
+++ php/get_patient.php
@@ -3,12 +3,12 @@ session_start();
 require '../db.php';
 echo "hello world<br>";
 if ($stmt = $mysqli->prepare("SELECT client_id FROM accounts_clients WHERE account_id=?")) {
+    $user_id = $_SESSION['user_id'];
+
+    $stmt->bind_param("i", $user_id);
     $stmt->execute();
 
-    $user_id = $_SESSION['user_id'];
-    $stmt->bind_param("i", $user_id);
     echo "user id is $user_id <br>";
-    echo "user id type is ". gettype($user_id);
     /* bind result variables */
     $stmt->bind_result($result);
 
