4ec07cff6faa18179aaf779fdea666f144240a8b
tom <tom.nijhof@cytosmart.com>
2018-03-17 17:15:01 +0100

    Multiple statements open at the same moment gives problems

--- php/get_patient.php
+++ php/get_patient.php
@@ -4,7 +4,7 @@ require '../db.php';
 echo "hello world<br>";
 // &&
 if ($stmt_acc_client = $mysqli->prepare("SELECT client_id FROM accounts_clients WHERE account_id=?")) {
-    if($stmt_client = $mysqli->prepare("SELECT first_name, last_name FROM clients WHERE id=?")){
+    if($stmt_client = $mysqli->prepare("SELECT first_name FROM clients WHERE id=?")){
         $results = array();
 
         $user_id = $_SESSION['user_id'];
@@ -20,25 +20,28 @@ if ($stmt_acc_client = $mysqli->prepare("SELECT client_id FROM accounts_clients 
         /* fetch value */
         while($stmt_acc_client->fetch()){
             echo "client $client_id<br>";
-            $stmt_client->bind_param("i", $client_id);
-            $stmt_client->execute();
-            $stmt_client->bind_result($first_name, $last_name);
 
-            if($stmt_client->fetch()){
-                $result = array($user_id, $first_name, $last_name);
-                echo "$first_name<br>";
-                print_r($result);
-                echo " <br>";
-            }else{
-                echo "error<br>";
-            }
+
+        }
+        $stmt_acc_client->close();
+
+        $stmt_client->bind_param("i", $client_id);
+        $stmt_client->execute();
+        $stmt_client->bind_result($first_name);
+        if($stmt_client->fetch()){
+            $result = array($user_id, $first_name);
+            echo "$first_name<br>";
+            print_r($result);
+            echo " <br>";
+        }else{
+            echo "error<br>";
         }
     }else{
         echo "ERROR: second statement";
     }
 
     /* close statement */
-    $stmt_acc_client->close();
+    $stmt_client->close();
 }else{
     echo "ERROR: first statement";
 }
